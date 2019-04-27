ce2cd030e4cd074d8f72e2b9fff344247fba92ba
AgaStupak <hane.aga@gmail.com>
2019-04-27 22:30:53 +0200

    lombok builder

--- src/main/java/agaStupak/App.java
+++ src/main/java/agaStupak/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Home home = Home.builder().Kitchen("kuchnia").Toilet("toaleta").LivingRoom("salon").build();
     }
 }
---
+++ src/main/java/agaStupak/Home.java
@@ -0,0 +1,10 @@
+package agaStupak;
+
+import lombok.Builder;
+
+@Builder
+public class Home {
+    String Kitchen;
+    String Toilet;
+    String LivingRoom;
+}
